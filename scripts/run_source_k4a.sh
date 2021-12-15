181dd2c1dde9d6638ede7a7b9c53ef5eec8fc41a
Gabor Soros <gabor.soros@nokia-bell-labs.com>
2021-12-15 17:13:47 +0100

    update k4a script with note on 16 vs 32 bit depth representations

--- scripts/run_source_k4a.sh
+++ scripts/run_source_k4a.sh
@@ -4,9 +4,13 @@ roslaunch \
 	azure_kinect_ros_driver driver.launch \
 	tf_prefix:=${HOSTNAME}_ \
 	color_resolution:=720P \
-	point_cloud:=true \
-	rgb_point_cloud:=true \
+	point_cloud:=false \
+	rgb_point_cloud:=false \
+	depth_enabled:=true \
 	depth_mode:=NFOV_UNBINNED \
+	depth_unit:=16UC1 \
 	fps:=15
 
-	
+# WARNING: depth in 16UC1 format is in millimeters, but depth in 32FC1 format is in meters! 
+# Make sure that the depth scale must be consistent in your pipeline.
+# see https://github.com/microsoft/Azure_Kinect_ROS_Driver/commit/ea70ccf
